4aa26079379ed84a0211dd36c7299802c847dc94
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2021-07-05 12:45:52 +0530

    Initial commit

---
+++ src/main/java/learn/spring/controller/HelloController.java
@@ -0,0 +1,18 @@
+package learn.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(name = "/hello", method = RequestMethod.GET)
+    public String hello(@RequestParam("name") String name, Model model) {
+        model.addAttribute("response", name);
+        return "hello";
+    }
+	
+}
